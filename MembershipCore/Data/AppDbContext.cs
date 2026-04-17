8c46de65cf3b9a71b5b4a1ef4185acfe931c4e30
Gabriel Campos <campgabriel813@gmail.com>
2026-04-17 14:20:25 -0600

    feat: configure EF Core with SQLite and add initial migration

--- MembershipCore/Data/AppDbContext.cs
+++ MembershipCore/Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 using MembershipCore.Models;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 
 namespace MembershipCore.Data
@@ -13,9 +14,27 @@ namespace MembershipCore.Data
         {
         }
 
+        //Tablas
         public DbSet<User> Users { get; set; }
-
         public DbSet<Membership> Memberships { get; set; }
+
+        //Configuración del modelo
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //Relacion de 1 a 1 entre User y Membership
+            //Un usuario tiene solo una membresía
+            modelBuilder.Entity<User>()
+                .HasOne(u  => u.Membership)
+                .WithOne(m  => m.User)
+                .HasForeignKey<Membership>(m  => m.UserId);
+
+            //Email unico por usuario
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+
 
     }
 }
