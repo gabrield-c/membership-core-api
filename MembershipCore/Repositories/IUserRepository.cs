de330c51f7b3a2976b9b7605a9eb086420746a93
Gabriel Campos <campgabriel813@gmail.com>
2026-04-19 22:38:43 -0600

    feat: add IUserRepository interface

---
+++ MembershipCore/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MembershipCore.Models;
+
+namespace MembershipCore.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();// Obtener todos los usuarios
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+    }
+}
